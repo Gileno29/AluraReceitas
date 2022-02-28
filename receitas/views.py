faebc89fe267ebd36a2629b347f2b068b474d671
gileno <gileno.cr.duarte@gmail.com>
2022-02-28 17:15:34 -0300

    adicionando campos dinamicos e configurando database

--- receitas/views.py
+++ receitas/views.py
@@ -3,8 +3,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+
 def index(request):
-    return render(request,'index.html')
+
+
+    receitas={
+        1:'lazanha',
+        2:'cuzcuz',
+        3:'salsicha ao molho',
+        4:'salmão'
+
+    }
+    dados= {
+        'nomes_das_receitas':receitas
+    }
+    return render(request,'index.html', dados)
 
 def receitas(request):
     return render(request, 'receita.html')

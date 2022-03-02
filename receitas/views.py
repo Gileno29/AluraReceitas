4b64d796d72c736470b42d453bfd88ae12a9bc5f
gileno <gileno.cr.duarte@gmail.com>
2022-03-02 12:31:00 -0300

    projeto inicialk finalizado

--- receitas/views.py
+++ receitas/views.py
@@ -13,10 +13,12 @@ def index(request):
     }
     return render(request,'index.html', dados)
 
+
 def receitas(request, receita_id):
     receita= get_object_or_404(Receita, pk=receita_id)
+   
     receita_a_exibir={
         'receita':receita
     }
-
+    
     return render(request, 'receita.html', receita_a_exibir)
